38251e84f4306ad37b1f43d08ce35c10308bea17
Elliot Voris <elliot@stellar.org>
2024-09-24 16:31:38 -0500

    add tsdoc comments to all functions and classes

--- src/base.ts
+++ src/base.ts
@@ -1,9 +1,24 @@
 import { SorobanRpc } from "@stellar/stellar-sdk"
 
+/**
+ * A base class that is used internally by the server and kit classes.
+ * @internal
+ */
 export class PasskeyBase {
+    /**
+     * The URL of the RPC server.
+     */
     public rpcUrl: string | undefined
+    /**
+     * The configured instance of the RPC server.
+     */
     public rpc: SorobanRpc.Server | undefined
-    
+
+    /**
+     * Create a new base passkey kit object.
+     *
+     * @param rpcUrl - The URL of the RPC server.
+     */
     constructor(rpcUrl?: string) {
         if (rpcUrl) {
             this.rpcUrl = rpcUrl
